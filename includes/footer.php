e24ba1527f47b0f6fa90f2e1a25bd019e13dd3be
saftaclaudia <saftaclaudia27@yahoo.com>
2014-03-09 00:32:11 +0200

    Initial commit

---
+++ includes/footer.php
@@ -0,0 +1,36 @@
+<div class="footercontent">
+ 		<div class="wrapper clearfix">
+ 			<div class="width25">
+ 				<h2>GET IN TOUCH</h2>
+ 				<p>Fusce dapibus, tellus commodo, tortor mauris condimentum utellus fermentum, porta sem malesuada magna. </p>
+ 				<address>
+ 					Moonshine Street No: 14/05 <br>
+					Light City, Jupiter<br>
+					0247 541 65 87<br>
+					support@longwave.com
+ 				</address>
+ 			</div>
+ 			<div class="width25">
+ 				<h2>TWITTER</h2>
+ 				<p>Aenean lacinia bibendum nulla sed leo posuere erat a ante venenatis dapibus posuere velit aliquet.</p>
+           		<p>Donec ullamcorper metus auctor fringilla. Nullam quis risus eget.</p>
+           		<p>Vestibulum id ligula porta  euismod semper. Maecenas faucibus mollis.</p>
+   			</div>
+        	<div class="width25">
+        		<h2>POPULAR POST</h2>
+        		<p>Vivamus sagittis lacus vel augue laoreet rutrum dolor auctor. 14 Nov, 2012 </p>
+        		<p>Scelerisque nisl consectetur et. 22 May, 2012 </p>
+        		<p>Pellentesque ornare sem lacinia quam venenatis vestibulum. Fusce 15 July, 2012</p>
+        	</div>
+        	<div class="width25">
+        		<h2>ABOUT US</h2>
+        		<p>Vestibulum id ligula porta felis euismod semper. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.</p>
+        		<p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus curabitur blandit tempus.</p>
+       	 	</div>
+		</div>
+    </div>
+    <div class="themereference">
+    	<div class="wrapper">
+    		<p class="allrights"> &copy 2013 Marble. All rights reserved. Theme by elemis.</p>
+    	</div>
+    </div>
---
+++ includes/header.php
@@ -0,0 +1,26 @@
+	<div class="wrapper">
+		<div class="header clearfix">
+			<b class="moose">Moose</b>
+			<ul class="contact clearfix">
+				<li class="nrphone"><p>0535 954 74 54 </p></li>
+				<li class="email"><a href="mailto:email@moose.com">email@moose.com</a></li>
+			</ul>
+		</div>	
+	</div>
+	<div class="headmenu clearfix">
+		<div class="wrapper clearfix">	
+			<ul class="menu">
+				<li class="men"><a href="homepage.html">HOME</a></li>
+				<li class="men"><a href="portfolio.html">PORTFOLIO</a></li>
+				<li class="men"><a href="services.html">SERVICES</a></li>
+				<li class="men"><a href="blog.html">BLOG</a></li>
+			</ul>		
+			<ul class="headico">
+				<li class="icorss"></li>
+				<li class="icofacebook"></li>
+				<li class="icotwitter"></li>
+				<li class="habarnam"></li>
+				<li class="pinterest"></li>
+			</ul>
+		</div>
+	</div>						
